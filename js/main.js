e1af2a5dcad1bc2d11c3041697b81ce88030300f
Simon O'Donovan <3421137+SimonODonovan@users.noreply.github.com>
2022-06-01 19:42:57 +0000

    Magic 8 ball API updates  - General   - Changed serverClean.js to server.js   - Remove otherpage & otherother html pages  - server.js   - Added 8ball answers array   - Updated handleApiRequest to take question querystring and return random 8ball answer  - index.html   - Updated style of page   - Added h2 elements to display user question and 8ball answer   - Updated button text   - Updated input placeholder  - main.js   - Updated fetch url to include user question   - Set user question and 8ball answer after fetch request

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
 
-  const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api?student=${userName}`)
+  const userQuestion = document.querySelector("#question").value;
+  const res = await fetch(`/api?question=${userQuestion}`)
   const data = await res.json()
+  
 
-  console.log(data);
-  document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
+  console.log(data.question);
+  document.querySelector("#questionHighlight").textContent = data.question
+  document.querySelector("#answer").textContent = data.answer
 }
